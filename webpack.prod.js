76940d97782df659e8a215c92ea1f7eb371c6b15
Dmitry Chizhov <chizhov.dee@gmail.com>
2017-10-31 03:04:00 +0500

    changed components structure, added production env to webpack prod config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack');
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
@@ -5,6 +6,11 @@ const common = require('./webpack.common.js');
 module.exports = merge(common, {
   devtool: 'source-map',
   plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
+      }
+    }),
     new UglifyJSPlugin({
       sourceMap: true
     })
